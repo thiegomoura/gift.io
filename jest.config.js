7f14616e299a3e0b4c58ed2568e462c9c5dbe52a
thiegomoura <thi-ego@hotmail.com>
2023-06-28 12:31:49 -0400

    change project build settings

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,17 @@
+const { pathsToModuleNameMapper } = require('ts-jest')
+// In the following statement, replace `./tsconfig` with the path to your `tsconfig` file
+// which contains the path mapping (ie the `compilerOptions.paths` option):
+const { compilerOptions } = require('./tsconfig')
+
+const nameMapper = pathsToModuleNameMapper(compilerOptions.paths /*, { prefix: '<rootDir>/' } */)
+
+console.log('nameMapper: ', nameMapper);
+
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-};
+  roots: ['<rootDir>'],
+  modulePaths: [compilerOptions.baseUrl], // <-- This will be set to 'baseUrl' value
+  moduleNameMapper: nameMapper,
+}
